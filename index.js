f20ba8eb72d0dac64b60c4f9a7bac35532d0ba98
Hussein Aladdin <haladdin13@outlook.com>
2023-12-11 20:21:48 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+    //returns the number of blocks given a value
+    if (distance < 42){
+      return(42 - distance);
+    }
+    else {
+  
+    return(distance - 42);
+  }
+  }
+
+function distanceFromHqInFeet (distance) {
+    return distanceFromHqInBlocks(distance)*264;
+}
+
+function distanceTravelledInFeet(a, b){
+    if (b > a){
+      return((b - a) * 264);
+    }
+    else
+      return((a - b) * 264);
+  }
+
+  function calculatesFarePrice(pickup, dropoff){
+    let distance = distanceTravelledInFeet(pickup, dropoff);
+    if (distance <= 400){
+      return 0;
+    }
+    if (distance >= 400 && distance <= 2000){
+      return (distance - 400) * 0.02;
+    }
+    else if(distance >= 2000 && distance <=2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return "cannot travel that far";
+    }
+  }
